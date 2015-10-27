506ee07045528acd48b6b7de6135843ddfe859bb
Bruno T. de Melo <btmg@cin.ufpe.br>
2015-10-27 17:08:03 -0400

    removed warnings

--- app/src/main/java/com/wico/ui/Main.java
+++ app/src/main/java/com/wico/ui/Main.java
@@ -1,12 +1,10 @@
 package com.wico.ui;
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -14,8 +12,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-import com.parse.Parse;
-import com.parse.ParseObject;
 import com.wico.R;
 import com.wico.network.NetworkChecker;
 import com.wico.network.ParseConnector;
@@ -39,7 +35,7 @@ public class Main extends AppCompatActivity {
         //set action button background
         connectText = (TextView) findViewById(R.id.connectmessage);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabNewQuestion);
-        int btncolor = getResources().getColor(R.color.colorAccent);
+        int btncolor = ContextCompat.getColor(this,R.color.colorAccent);
         fab.setBackgroundTintList(ColorStateList.valueOf(btncolor));
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
