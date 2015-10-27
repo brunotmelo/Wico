51617dc26768e58880b82590ac5f0f0735b80cca
Jack <jtmcginnis@bsu.edu>
2015-10-27 16:38:06 -0600

    Cleaned up the code

--- app/src/main/java/com/wico/datatypes/Question.java
+++ app/src/main/java/com/wico/datatypes/Question.java
@@ -1,22 +1,33 @@
 package com.wico.datatypes;
 
 import com.parse.ParseClassName;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.wico.exceptions.WicoParseException;
+
+import java.util.ArrayList;
 
 @ParseClassName("Question")
 public final class Question extends ParseObject{
 
-    public static final class Builder {
+    public static final class Builder{
         private String title;
         private String content;
+        private int numberOfAnswers;
 
-        public Builder title(String title) {
+        public Builder title(String title){
             this.title = title;
             return this;
         }
 
-        public Builder content(String content) {
+        public Builder content(String content){
             this.content = content;
+            return this;
+        }
+
+        public Builder numberOfAnswers(int numberOfAnswers){
+            this.numberOfAnswers = numberOfAnswers;
             return this;
         }
 
@@ -25,25 +36,40 @@ public final class Question extends ParseObject{
         }
     }
 
+    /** The method below (public Question()) , although empty, is mandatory to allow question object to be used as a parse object.
+     */
     public Question(){
-        //this method required to use it as a parseObject
-    }
-    private Question(Builder builder) {
-        put("title", builder.title);
-        put("content",builder.content);
+
     }
 
-    public String getTitle() {
+    private Question(Builder builder){
+        put("title", builder.title);
+        put("content",builder.content);
+        put("answers",builder.numberOfAnswers);
+    }
+
+    public String getTitle(){
         return getString("title");
     }
 
-    public String getContent() {
+    public String getContent(){
         return getString("content");
     }
 
-    public int getNumOfAwnsers(){
-        return 5;
+    public String getNumberOfAwnsers(){
+        return getString("numberOfAnswers");
     }
 
-
+    public ArrayList<Question> saveArrayOfQuestions(){
+        ParseQuery<Question> query = ParseQuery.getQuery(Question.class);
+        query.whereExists("content");
+        query.orderByDescending("createdAt");
+        ArrayList<Question> questions = new ArrayList<>();
+        try{
+            questions.addAll(query.find());
+        }catch (ParseException exception) {
+            throw new WicoParseException();
+        }
+         return questions;
+    }
 }
--- app/src/main/java/com/wico/network/ParseConnector.java
+++ app/src/main/java/com/wico/network/ParseConnector.java
@@ -11,74 +11,20 @@ import java.util.ArrayList;
 
 public class ParseConnector {
 
-
     public ParseConnector(){
+
     }
 
     public void storeQuestion(Question question){
         try {
             question.save();
-        } catch (ParseException e) {
+        } catch (ParseException exception) {
             throw new WicoParseException();
         }
     }
 
 
 
-    public ArrayList<Question> getQuestions(){
-        ParseQuery<Question> query = ParseQuery.getQuery(Question.class);
-        query.whereExists("content");
-        query.orderByDescending("createdAt");
-        ArrayList<Question> questions = new ArrayList<>();
-        try{
-           questions.addAll(query.find());
-        }catch (ParseException e) {
-            throw new WicoParseException();
-        }
-
-        return questions;
-
-    }
-
-
-
-
-    /*private void testQuestionStorage(){
-        Question testQuestion = new Question(content, title);
-        ParseObject testObject = new ParseObject("Question");
-        testObject.put("content", content);
-        testObject.put("title",title);
-        testObject.put();
-        testObject.saveInBackground();
-    }
-    private void testQueryForContent(){
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Question");
-        query.whereExists("content");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> list, ParseException e) {
-                if (e == null) {
-                    Log.d("content", "Retrieved" + content);
-                } else {
-                    Log.d("content", "Error: " + e.getMessage());
-                }
-            }
-        });
-    }
-    private void testQueryForTitle(){
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Title");
-        query.whereExists("title");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> list, ParseException e) {
-                if (e == null) {
-                    Log.d("title", "Retrieved" + content);
-                } else {
-                    Log.d("title", "Error: " + e.getMessage());
-                }
-            }
-        });
-    }*/
 
 
 }
